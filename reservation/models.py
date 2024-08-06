fb5509ce021c44b647e2a41362687cacf49138d1
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-06 17:15:01 +0000

    Add reservation model to reservation.model.py

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "confirmed"), (1, "cancelled"))
 
 # Create your models here.
+class reservation(models.Model):
+    reservation_num = models.IntegerField(unique=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    num_of_guests = models.IntegerField()
+    allergies = models.TextField()
+    special_reqs = models.TextField()
+    reservation_status = models.IntegerField(choices= STATUS)
+    created_on = models.DateTimeField(auto_now_add=True)
+
