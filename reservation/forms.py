53a1bfe1acbb5b31dcbdf18d73b7e90cdeea8f35
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-07 23:11:17 +0000

    update reservation_create function to allow onlu logged in users to create reservations with their unique id

--- reservation/forms.py
+++ reservation/forms.py
@@ -4,4 +4,4 @@ from .models import reservation
 class reservationForm(forms.ModelForm):
     class Meta:
         model = reservation
-        fields = ['user_id', 'date', 'time', 'number_of_guests', 'allergies', 'special_requirements']
+        fields = ['date', 'time', 'number_of_guests', 'allergies', 'special_requirements']
--- reservation/views.py
+++ reservation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import reservation
 from .forms import reservationForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -11,11 +12,15 @@ def reservation_list(request):
     reservations = reservation.objects.all()
     return render(request, 'reservation/reservation_list.html', {'reservations': reservations})
 
+
+@login_required #Only allow logged in users to create a reservation with their unique id
 def reservation_create(request):
     if request.method == 'POST':
         form = reservationForm(request.POST)
         if form.is_valid():
-            form.save()
+            reservation = form.save(commit=False)
+            reservation.user_id = request.user
+            reservation.save()
             return redirect('reservation_list')
     else:
         form = reservationForm()
