781a9bdaa445250884c87bfe20fa059900c87497
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-08 14:23:10 +0000

    fix import 404 in reservation.views.py

--- reservation/views.py
+++ reservation/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Reservation
 from .forms import ReservationForm
 from django.contrib.auth.decorators import login_required
