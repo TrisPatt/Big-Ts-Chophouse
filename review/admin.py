1b0c1bee8c5e5bb9793b2e2306997ee63790998a
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-15 20:25:44 +0000

    fix review model registration in admin.py

--- review/admin.py
+++ review/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
+from .models import Review
 
 @admin.register(review)
-class reviewAdmin(SummernoteModelAdmin):
+class ReviewAdmin(SummernoteModelAdmin):
     list_display = ('user', 'overall_rating', 'comment', 'creaed_on')
