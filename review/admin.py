4b4712a6670bb0d5e746766cba71d563aabcc0dd
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-15 20:30:49 +0000

    Fix SummernoteModelAdmin import in admin.py

--- review/admin.py
+++ review/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Review
+from django_summernote.admin import SummernoteModelAdmin 
 
 @admin.register(Review)
 class ReviewAdmin(SummernoteModelAdmin):
