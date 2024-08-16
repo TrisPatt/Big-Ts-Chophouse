8fff5ec46cc6913afc745c332386bbc3ca0390b9
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-16 18:18:23 +0000

    add logic to ReviewForm function in review.forms.py in order to validate date to throw error if date is in the future

--- review/forms.py
+++ review/forms.py
@@ -1,8 +1,26 @@
 from django import forms
 from .models import Review
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['visit_date', 'overall_rating', 'food_rating', 'service_rating', 'ambience_rating', 'comment']
+        widgets = {
+            'visit_date': forms.DateInput(attrs={
+                'class': 'form-control datepicker',
+                'id': 'reservation-date',
+                'data-provide': 'datepicker',
+                'data-date-format': 'yyyy-mm-dd',
+            })
+        }
+    def clean_visit_date(self):
+        visit_date = self.cleaned_data.get('visit_date')
 
+        # Ensure the visit date is not in the future
+        if visit_date and visit_date > timezone.now().date():
+            raise ValidationError("The visit date cannot be in the future.")
+        
+        return visit_date
