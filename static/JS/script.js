03c63f4f868f3832cee981711bd6150a31fef8c6
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-16 15:50:45 +0000

    add custom styles to style.css for navbar links effects

---
+++ static/JS/script.js
@@ -0,0 +1,11 @@
+///Date picker///
+$(document).ready(function(){
+    $('#reservation-date').datepicker({
+      format: 'yyyy-mm-dd',
+      autoclose: true,
+      todayHighlight: true,
+      startDate: new Date()
+    });
+  });
+
+  
