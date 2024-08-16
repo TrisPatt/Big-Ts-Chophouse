a227b05a62038e771826c5a97b9b461f019a72ea
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-16 12:15:20 +0000

    add edit_review.html to review templates

--- review/urls.py
+++ review/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('submit_review/', views.submit_review, name='submit_review'),
     path('reviews/', views.review_list, name='review_list'),
+    path('reviews/edit/<int:review_id>/', views.edit_review, name='edit_review'),
 ]
