9d48093eb218805c6dd3363a144fe2d97896d703
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-16 11:36:12 +0000

    add edit_review function to review.views.py with user specific permissions

--- review/views.py
+++ review/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Review
 from .forms import ReviewForm
+from django.core.exceptions import PermissionDenied
+from django.contrib import messages
 
 @login_required
 def submit_review(request):
@@ -11,6 +13,7 @@ def submit_review(request):
             review = form.save(commit=False)
             review.user = request.user
             review.save()
+            messages.success(request, 'Thank you. Your review has been submitted')
             return redirect('review_list')  
     else:
         form = ReviewForm()
@@ -20,3 +23,22 @@ def review_list(request):
     reviews = Review.objects.all().order_by('-created_on')
     rating_range = range(1, 6)  
     return render(request, 'review/review_list.html', {'reviews': reviews, 'rating_range': rating_range})
+
+@login_required
+def edit_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+
+    # Check if the user is the owner of the review
+    if review.user != request.user:
+        raise PermissionDenied
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you. Your review has been updated') # Display confirmation message
+            return redirect('review_list')  # Redirect to the review list after editing
+    else:
+        form = ReviewForm(instance=review)
+    
+    return render(request, 'review/edit_review.html', {'form': form})
