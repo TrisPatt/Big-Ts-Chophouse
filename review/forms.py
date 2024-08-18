ad55ea031d3a73716e5d82456bdc89b34adaea78
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-18 12:16:22 +0000

    add bootsrap carousel to home.html and style.css with placeholder images

--- review/forms.py
+++ review/forms.py
@@ -7,7 +7,7 @@ from django.core.exceptions import ValidationError
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['visit_date', 'overall_rating', 'food_rating', 'service_rating', 'ambience_rating', 'comment']
+        fields = ['title', 'visit_date', 'overall_rating', 'food_rating', 'service_rating', 'ambience_rating', 'comment']
         widgets = {
             'visit_date': forms.DateInput(attrs={
                 'class': 'form-control datepicker',
