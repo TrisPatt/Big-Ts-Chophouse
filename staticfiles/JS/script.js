19a6957a8d01b2a5db5f7981f1e7dbbeabf6853b
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-24 08:35:48 +0000

    collect static files

--- staticfiles/JS/script.js
+++ staticfiles/JS/script.js
@@ -1,11 +1,26 @@
 ///Date picker///
+
 $(document).ready(function(){
     $('#reservation-date').datepicker({
       format: 'yyyy-mm-dd',
       autoclose: true,
       todayHighlight: true,
-      startDate: new Date()
+      startDate: new Date(),
+      templates: {
+        leftArrow: '<i class="fas fa-chevron-left"></i>',
+        rightArrow: '<i class="fas fa-chevron-right"></i>',
+      }
     });
-  });
+});
 
-  
+$(document).ready(function(){
+  $('#visit-date').datepicker({
+    format: 'yyyy-mm-dd',
+    autoclose: true,
+    templates: {
+      leftArrow: '<i class="fas fa-chevron-left"></i>',
+      rightArrow: '<i class="fas fa-chevron-right"></i>',
+    },
+    endDate: new Date(),
+  });
+});
