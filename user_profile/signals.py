1f6ea1fbd95e9ccfeae7d621b38ac46e399f7231
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-29 23:27:35 +0000

    fix format of user_profile.signals and urls to comply with pep8 guidelines

--- user_profile/signals.py
+++ user_profile/signals.py
@@ -3,10 +3,20 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
+    """
+    Signal receiver that ensures a Profile is created for each User instance.
+
+    This function is triggered whenever a User instance is saved. If the User
+    is newly created, it attempts to create a corresponding Profile. If the
+    User already existed but didn't have an associated Profile, it will create
+    one to ensure consistency.
+
+    """
     if created:
         Profile.objects.get_or_create(user=instance)
-    else:      
+    else:
         if not hasattr(instance, 'profile'):
             Profile.objects.create(user=instance)
