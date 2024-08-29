cdfa2dea7d9d415e62d52c8ee32467c3b066d903
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-29 14:04:41 +0000

    Add comments to reservation.views.py

--- reservation/views.py
+++ reservation/views.py
@@ -10,32 +10,62 @@ from django.contrib.auth.decorators import login_required
 from datetime import date
 
 
-# home page
 def home(request):
+    """
+    Render the home page
+    """
     return render(request, 'reservation/home.html')
 
 
 # Confirmed reservation
 def reservation_confirmed(request, reservation_number):
+    """
+    Render the reservation confirmation page.
+
+    Fetches and displays the reservation details based on the provided
+    reservation number.
+
+    Args:
+        reservation_number: The unique identifier for the reservation.
+
+    Returns:
+        A rendered HTML page with reservation details.
+    """
     reservation = get_object_or_404(Reservation, pk=reservation_number)
     return render(request, 'reservation/reservation_confirmed.html',
                   {'reservation': reservation})
 
 
-# review reservations
 @login_required
 def reservation_list(request):
+    """
+    Display a list of reservations for the logged-in user.
+
+    Fetches and displays reservations made by the logged-in user,
+    sorted by date in descending order.
+
+    Returns a rendered HTML page with a list of reservations.
+    """
     if request.user.is_authenticated:
         reservations = Reservation.objects.filter(user_id=request.user.id).order_by('-date')
         return render(request, 'reservation/reservation_list.html',
                       {'reservations': reservations})
     else:
-        # Redirect to login page if the user is not authenticated
         return redirect('templates/account/login.html')
 
 
 @login_required
 def reservation_create(request):
+    """
+    Handle the creation of a new reservation.
+
+    Handles the reservation creation form, checks table availability,
+    and sends a confirmation email to the user. If the reservation cannot
+    be accommodated, an error message is shown.
+
+    Returns a rendered HTML page with the reservation creation form or a redirect
+    to the confirmation page upon successful creation.
+    """
     if request.method == 'POST':
         form = ReservationForm(request.POST, user=request.user)
         if form.is_valid():
@@ -43,11 +73,9 @@ def reservation_create(request):
             reservation.user_id = request.user
             reservation.save()
 
-            # Calculate how many tables of 2 are needed
             num_guests = reservation.number_of_guests
             tables_needed = (num_guests + 1) // 2
 
-            # Calculate available tables on a partcular date and time
             available_tables = Table.objects.annotate(
                 reserved_count=models.Count('reservations', filter=models.Q(
                     reservations__date=reservation.date,
@@ -69,7 +97,6 @@ def reservation_create(request):
                 reservation.tables.add(*tables)
                 reservation.save()
 
-                # sends email to user- set as backend
                 send_mail(
                 'Reservation Confirmed!',
                 f"""Your reservation on {reservation.date} at {reservation.time} 
@@ -93,10 +120,17 @@ def reservation_create(request):
 
 
 def available_time_slots(request):
+    """
+    Display available time slots based on the selected date.
+
+    Retrieves the available time slots for reservations on the selected
+    date, excluding fully booked slots.
+
+    Returns a rendered HTML page with available time slots.
+    """
     selected_date = request.GET.get('date', date.today())
     all_time_slots = TimeSlot.objects.all()
 
-    # Find booked time slots that are full (24 or more guests)
     booked_slots = (
     Reservation.objects
     .filter(date=selected_date, reservation_status=0)
@@ -104,7 +138,6 @@ def available_time_slots(request):
     .annotate(total_guests=Sum('number_of_guests'))
     .filter(total_guests__gte=24)
 )
-    # Exclude fully booked slots
     available_slots = all_time_slots.exclude(id__in=[slot['time']
                                                      for slot in booked_slots])
 
@@ -114,6 +147,19 @@ def available_time_slots(request):
 
 @login_required
 def cancel_reservation(request, reservation_number):
+    """
+    Handle the cancellation of an existing reservation.
+
+    Fetches the reservation based on the reservation number, updates its
+    status to 'cancelled', and sends a cancellation confirmation email to
+    the user. Redirects to the reservation list page upon success.
+
+    Args:
+        reservation_number: The unique identifier for the reservation.
+
+    Returns a rendered HTML page to confirm cancellation and a redirect to
+    the reservation list page upon successful cancellation or cancelled cancellation.
+    """
     reservation = get_object_or_404(
         Reservation, reservation_number=reservation_number, user_id=request.user
     )
@@ -141,8 +187,21 @@ def cancel_reservation(request, reservation_number):
 
 @login_required
 def update_reservation(request, reservation_number):
+    """
+    Handle the update of an existing reservation.
+
+    Fetches the reservation based on the reservation number, validates
+    the updated reservation details, checks table availability, and
+    sends a confirmation email for the updated reservation. Redirects to
+    the confirmation page upon successful update.
+
+    Args:
+        reservation_number: The unique identifier for the reservation.
+
+    Returns a rendered HTML page with the reservation update form or a redirect
+    to the confirmation page upon successful update.
+    """
     try:
-        # Fetch the reservation for the logged-in user
         reservation = Reservation.objects.get(reservation_number=reservation_number)
         print(f"Logged-in user: {request.user}")
         print(f"Reservation user: {reservation.user_id}")
@@ -150,7 +209,6 @@ def update_reservation(request, reservation_number):
         print("Reservation does not exist")
         raise
 
-    # Ensure the reservation belongs to the logged-in user
     if reservation.user_id != request.user:
         print("Reservation does not belong to the logged-in user.")
         return redirect('reservation_list')
@@ -162,10 +220,8 @@ def update_reservation(request, reservation_number):
             updated_reservation.user_id = request.user
 
             num_guests = updated_reservation.number_of_guests
-            tables_needed = (num_guests + 1) // 2  # Calculate tables needed
+            tables_needed = (num_guests + 1) // 2  
 
-            # Check for table availability at the updated date and time,
-            # excluding current reservation
             total_guests_at_time = Reservation.objects.filter(
             date=updated_reservation.date,
             time=updated_reservation.time,
@@ -180,7 +236,6 @@ def update_reservation(request, reservation_number):
                 return render(request, 'reservation/reservation_form.html',
                               {'form': form, 'reservation': reservation})
 
-            # Handle table assignment based on the update
             available_tables = Table.objects.annotate(
                 reserved_count=models.Count('reservations', filter=models.Q(
                     reservations__date=updated_reservation.date,
@@ -190,8 +245,6 @@ def update_reservation(request, reservation_number):
             ).filter(reserved_count=0).count()
 
             if tables_needed <= available_tables:
-                # Clear existing tables and assign new ones
-                # based on updated details
                 updated_reservation.tables.clear()
 
                 available_tables = Table.objects.annotate(
@@ -206,7 +259,6 @@ def update_reservation(request, reservation_number):
                 updated_reservation.tables.add(*available_tables)
                 updated_reservation.save()
 
-                # Send a confirmation email for the updated reservation
                 send_mail(
                     'Reservation Updated!',
                     f"""Your reservation has been updated to
