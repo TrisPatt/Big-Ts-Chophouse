0446067201c66064a91217eec87e62cd82c7cb5c
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-29 23:11:39 +0000

    fix format of user_profile.admin and forms.py to comply with pep8 guidelines

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Profile
 
-# Register your models here.
+
 admin.site.register(Profile)
 
+
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'first_name', 'last_name', 'email_address', 'phone_number')
+    """
+    The ProfileAdmin class customizes the admin view for the Profile model,
+    displaying the additional user details in the list view.
+    """
+    list_display = ('user', 'first_name', 'last_name', 'email_address',
+                    'phone_number')
--- user_profile/forms.py
+++ user_profile/forms.py
@@ -2,35 +2,60 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 class UserForm(forms.ModelForm):
+    """
+    A form for updating the User model's basic information.
+
+    This form includes fields for first name, last name, and email,
+    which are all attributes of the built-in Django User model.
+    """
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
 
+
 class ProfileForm(forms.ModelForm):
+    """
+    A form for updating the Profile model's additional user information.
+
+    This form includes fields for phone number and date of birth,
+    which are attributes of the custom Profile model.
+    """
     class Meta:
         model = Profile
         fields = ['phone_number', 'date_of_birth']
 
 
 class CombinedUserProfileForm(forms.Form):
-    # Fields from User model
+    """
+    A form for updating both User and Profile model data simultaneously.
+
+    This form combines fields from both the User model (first name, last name,
+    email)
+    and the Profile model (phone number, date of birth) into a single form.
+
+    """
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
     email = forms.EmailField()
-    
-    # Fields from Profile model
+
     phone_number = forms.CharField(max_length=30, required=False)
-    date_of_birth = forms.DateField(required=False, help_text='(Format: yyyy-mm-dd)')
-    
+    date_of_birth = forms.DateField(
+        required=False, help_text='(Format: yyyy-mm-dd)')
+
     def __init__(self, *args, **kwargs):
-        # Instance of the User and Profile objects
+        """
+        Initializes the form with user and profile instances.
+
+        If instances of User and Profile are provided, the form fields
+        are pre-populated with their respective data.
+        """
         self.user = kwargs.pop('user', None)
         self.profile = kwargs.pop('profile', None)
         super().__init__(*args, **kwargs)
-        
+
         if self.user and self.profile:
-            # Populate form fields with existing data
             self.fields['first_name'].initial = self.user.first_name
             self.fields['last_name'].initial = self.user.last_name
             self.fields['email'].initial = self.user.email
@@ -38,15 +63,20 @@ class CombinedUserProfileForm(forms.Form):
             self.fields['date_of_birth'].initial = self.profile.date_of_birth
 
     def save(self):
-        # Save the user data
+        """
+        Saves the form data to both User and Profile models.
+
+        The user-related data is saved to the User model, and the
+        profile-related data is saved to the Profile model.
+        """
         if self.user:
             self.user.first_name = self.cleaned_data['first_name']
             self.user.last_name = self.cleaned_data['last_name']
             self.user.email = self.cleaned_data['email']
             self.user.save()
-        
-        # Save the profile data
+
         if self.profile:
             self.profile.phone_number = self.cleaned_data['phone_number']
             self.profile.date_of_birth = self.cleaned_data['date_of_birth']
             self.profile.save()
+            
