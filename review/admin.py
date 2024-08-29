24430cd5367cd0adfb45eb72beda09a394f5385b
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-08-29 14:18:10 +0000

    add comments to review.forms.py

--- review/admin.py
+++ review/admin.py
@@ -5,4 +5,12 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Review)
 class ReviewAdmin(SummernoteModelAdmin):
+    """
+    Admin configuration for the Review model.
+
+    This class customizes the Django admin interface for the Review model
+    by using the Summernote WYSIWYG editor for rich text fields.
+    
+    """
+
     list_display = ('user', 'overall_rating', 'comment', 'created_on')
--- review/forms.py
+++ review/forms.py
@@ -5,6 +5,17 @@ from django.core.exceptions import ValidationError
 
 
 class ReviewForm(forms.ModelForm):
+    """
+    A form for submitting reviews.
+
+    This form is used to collect and validate review information from users.
+    It includes fields for various aspects of the review, such as ratings and comments,
+    and includes validation to ensure that the visit date is not set in the future.
+
+    Uses a DateInput widget with the 'form-control datepicker' class and an ID 
+    of 'visit-date' for date selection.
+
+    """
     class Meta:
         model = Review
         fields = ['title', 'visit_date', 'overall_rating', 'food_rating',
@@ -19,8 +30,15 @@ class ReviewForm(forms.ModelForm):
 
     def clean_visit_date(self):
         visit_date = self.cleaned_data.get('visit_date')
+        """
+        Validates that the visit date is not in the future.
 
-        # Ensure the visit date is not in the future
+        This method is called during form validation to ensure that the visit date
+        entered by the user is not a future date. If the date is in the future,
+        a ValidationError is raised.
+
+        """
+
         if visit_date and visit_date > timezone.now().date():
             raise ValidationError("The visit date cannot be in the future.")
 
